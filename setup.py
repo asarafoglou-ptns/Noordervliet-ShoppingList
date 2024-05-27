63ca6eaed3eafd77e8b8ee3b79bcce05bb6ead7f
Bamske <amber.noordervliet@student.uva.nl>
2024-05-27 17:38:44 +0200

    Week 2
    Added HTML pages,
    Made sure a data base is created,
    now the page accepts creating an account

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = "ShoppingList",
+    version = "0.0.1",
+    author = "Amber",
+    author_email= "amber.noordervliet@student.uva.nl",
+    python_requires = ">3.6",
+    packages = setuptools.find_packages()
+)
