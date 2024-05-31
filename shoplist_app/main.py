2626df4bed952a71b8c2291998d820e9d3412966
Bamske <157971554+Bamske@users.noreply.github.com>
2024-05-31 10:17:22 +0200

    Update main.py
    Added docstrings

--- shoplist_app/main.py
+++ shoplist_app/main.py
@@ -10,22 +10,32 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 def create_app():
+"""
+Function that creates/initializes the webapplication. Takes no arguments and returns app; the webapplication
+Usage: should be called to in the run file with flask_app = create_app()
+"""
 
+  #Configure the app and initialize the database
   app = Flask(__name__, template_folder="templates", static_folder="static")
   app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
   app.config["SECRET_KEY"] = "secret-key"
   
   db.init_app(app)
 
+  #Define the login manager system
   login_manager = LoginManager()
   login_manager.init_app(app)
 
   from models import User
 
-  #Defines how the login manager loads the user
   @login_manager.user_loader
   def load_user(id):
-      return User.query.get(id)
+    """
+    Defines how the login manager loads the user, in this case by user id.
+    :param id: user id
+    :return: user information from database
+    """
+    return User.query.get(id
   
   bcrypt = Bcrypt(app)
 
@@ -33,7 +43,7 @@ def create_app():
   from routes import register_routes
   register_routes(app, db, bcrypt)
 
-  #Usually done in terminal? Only needed once?
+  #Creates the database with the model class tables
   with app.app_context():
     db.create_all()
 
