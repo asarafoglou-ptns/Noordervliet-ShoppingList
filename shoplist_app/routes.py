66e04827e7d5c02ab03befe27ee80aafdfc69dbc
Bamske <157971554+Bamske@users.noreply.github.com>
2024-05-31 10:37:28 +0200

    Update routes.py
    Added docstrings

--- shoplist_app/routes.py
+++ shoplist_app/routes.py
@@ -8,14 +8,31 @@ from sqlalchemy import exc
 
 
 def register_routes(app, db, bcrypt):
+    """
+    Registers the differen routes (web pages) of the application.
+    Is automatically called on in create_app(), no need to change the arguments if not changing the name of app, db, and bycrypt in main.
+    :param app: app created in main.py
+    :param db: database created in main.py
+    :param bcrypt: bycrypt encryption created in main.py
+    :return: different html pages
+    """
     
     @app.route("/", methods=["GET", "POST"])
     def home():
+    """
+    Returns the template for the home page.
+    """
         return render_template("home.html")
     
     @app.route("/shoppinglist", methods=["GET", "POST"])
+    @login_required # Page only visible when logged in
     def shoppinglist():
+    """
+    Returns the template for the shoppinglist page.
+    Determines the type of request being made. If just a GET it will render the page, if a POST it will add the information into the database and return the updated page.
+    """
         if request.method == "POST":
+            # Get information from the form and add new item to the database
             staple = request.form.get("staple")
             grocery = request.form.get("grocery")
             if staple:
@@ -34,6 +51,9 @@ def register_routes(app, db, bcrypt):
     
     @app.route('/delete-staple', methods=['POST'])
     def delete_staple():  
+    """
+    Deletes a staple from the data base and the shoppinglist page. Returns through a javascript function the updated shoppinglist page. 
+    """
         staple = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
         stapleId = staple['stapleId']
         staple = Staple.query.get(stapleId)
@@ -46,6 +66,10 @@ def register_routes(app, db, bcrypt):
     
     @app.route('/delete-grocery', methods=['POST'])
     def delete_grocery():  
+    """
+    Deletes a grocery from the data base and the grocerylist, if item was a staple it is returned to the staple list and database. 
+    Returns through a javascript function the updated shoppinglist page. 
+    """
         grocery = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
         groceryId = grocery['groceryId']
         grocery = Grocery.query.get(groceryId)
@@ -64,6 +88,11 @@ def register_routes(app, db, bcrypt):
     
     @app.route('/check-staple', methods=['POST'])
     def check_staple():  
+    """
+    Deletes a pantry staple from the data base and the shoppinglist when it is checked of on the shopppinglist page.
+    It adds the item to the database as a grocery and adds it to the grocery list.
+    Returns through a javascript function the updated shoppinglist page. 
+    """
         staple = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
         stapleId = staple['stapleId']
         staple = Staple.query.get(stapleId)
@@ -79,6 +108,12 @@ def register_routes(app, db, bcrypt):
         
     @app.route("/signup", methods=["GET", "POST"])
     def signup():
+    """
+    Signs a user up. 
+    Function checks the form input. 
+    If invalid returns the signup page with error messages.
+    If valid, then creates a hash for the password and saves the information for the user in the database and returns the login page. 
+    """
         if current_user.is_authenticated:
             return redirect(url_for('home'))
         form = signupForm()
@@ -92,6 +127,12 @@ def register_routes(app, db, bcrypt):
 
     @app.route("/login", methods=["GET", "POST"])
     def login():
+    """
+    Logs a user in. 
+    Function checks the form input. 
+    If invalid returns the login page with error messages.
+    If valid, then checks for an account with that information and logs the user in and returns the shoppinglist page. 
+    """
         if current_user.is_authenticated:
             return redirect(url_for('home'))
         form = loginForm()
@@ -105,8 +146,12 @@ def register_routes(app, db, bcrypt):
         return render_template('login.html', title='Login', form=form)
             
     @app.route("/logout")
-    @login_required
+    @login_required # Page only visible when logged in
     def logout():
+    """
+    Logs a user out. 
+    Function logs out user and returns the home page. 
+    """
         logout_user()
         return render_template("home.html")
                 
